624824a2826d3bfc4ef6ee63021e7c4bbc1728db
Park Donghyeon <emma0415@naver.com>
2020-05-14 03:25:52 +0900

    CMake 빌드 모드 분리
    * Release 모드 빌드 시 NDEBUG 매크로 활성화(표준)
    * Debug 모드 빌드 시 DEBUG 매크로 활성화(비표준)

---
+++ README.md
@@ -0,0 +1,10 @@
+# CPP Project Templete with CMake
+
+Include
+* googletest
+* spdlog
+
+Download
+```bash
+$ git clone --recursive https://github.com/ppd0523/cpp-templete.git
+```
--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,14 @@ int main(int argc, char *argv[])
     /* code */
     std::cout << "hello " << std::endl;
     spdlog::info("{}", "world");
+
+#ifdef DEBUG
+    spdlog::info("Debug macro");
+#endif
+
+#ifdef RELEASE
+    spdlog::info("Release macro");
+#endif
+
     return 0;
 }
